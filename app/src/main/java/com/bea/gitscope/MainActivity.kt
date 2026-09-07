9a1ca944a7e845c5ba41e422e7d7eacc003c5166
bea09 <bea09bea@gmail.com>
2026-09-07 18:28:20 +0200

    tab navigation works

--- app/src/main/java/com/bea/gitscope/MainActivity.kt
+++ app/src/main/java/com/bea/gitscope/MainActivity.kt
@@ -1,26 +1,74 @@
 package com.bea.gitscope
 
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import android.widget.FrameLayout
+import android.widget.TableLayout
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.bea.gitscope.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Visa home direkt när appen startar
+        if (savedInstanceState == null ) {
+            replaceFragment(HomeFragment())
+        }
+
+        binding.tabLayout.addOnTabSelectedListener(
+            object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    when(tab?.position) {
+                        0 -> replaceFragment(HomeFragment())
+                        1 -> replaceFragment(SearchFragment())
+                        2 -> replaceFragment(ProfileFragment())
+                        3 -> replaceFragment(FavoritesFragment())
+
+                    }
+                }
+
+                override fun onTabUnselected(p0: TabLayout.Tab?) {
+
+                }
+
+                override fun onTabReselected(p0: TabLayout.Tab?) {
+
+                }
+            }
+        )
 
 
-
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
             insets
         }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer,fragment)
+            .commit()
     }
 }
